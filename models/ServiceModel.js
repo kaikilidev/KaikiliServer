b33d14b7d4663770ce9265a8bd563f7f36a0484b
Administrator <kailashborachiya@gmail.com>
2019-02-15 16:52:58 +0530

    Add Service Update

--- models/ServiceModel.js
+++ models/ServiceModel.js
@@ -6,7 +6,16 @@ var config = require('../db_config.json');
 
 var Service = {
 
-
+// MongoClient.connect(url, function(err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("KaikiliService");
+//     var myobj = { serviceName: "TV Mount Installation", serviceId: "1", serviceInfo: "tv service information ", isActive : 1 };
+//     dbo.collection("Service").insertOne(myobj, function(err, res) {
+//         if (err) throw err;
+//         console.log("1 document inserted");
+//         db.close();
+//     });
+// });
 };
 
 
