9870d898e3f31dc566ee3092162ef9e314b9e42d
Administrator <kailashborachiya@gmail.com>
2019-02-15 19:17:54 +0530

    Add Service Update

--- models/UserServiceModel.js
+++ models/UserServiceModel.js
@@ -11,16 +11,19 @@ var UserService = {
         var sp_id = req.body.sp_id;
         var sr_id = req.body.sr_id;
         var recodeId = "";
+
         mongo.connect(config.dbUrl, function (err, db) {
             var collection = db.db(config.dbName).collection(config.collections.sp_sr_catalogue);
-            var addServiceArray = collection.find({sp_id: sp_id, sr_id: sr_id}).toArray();
+            console.log(sp_id);
+            console.log(sr_id);
 
-            console.log(addServiceArray.length);
-            if (addServiceArray.length > 0) {
-                var addServiceArray = collection.find({sp_id: sp_id, sr_id: sr_id}).toArray(function (err, docs) {
+            collection.find({sp_id: sp_id, sr_id: sr_id}).toArray(function (err, docs){
 
+                if(docs.length >0){
                     recodeId = docs[0]._id;
                     console.log(recodeId);
+
+                    // Update service record
                     collection.update({_id: recodeId}, {$set: addService}, function (err, records) {
                         if (err) {
                             console.log(err);
@@ -33,37 +36,42 @@ var UserService = {
                         } else {
                             var status = {
                                 status: 1,
-                                message: "Success upload to sub service to server",
+                                message: "Success upload to old sub service to server",
                                 // data: records['ops'][0]
                             };
                             console.log(status);
                             callback(status);
                         }
                     });
-                });
-            } else {
-                collection.insert(addService, function (err, records) {
-                    if (err) {
-                        console.log(err);
-                        var status = {
-                            status: 0,
-                            message: "Failed"
-                        };
-                        console.log(status);
-                        callback(status);
-                    } else {
-                        var status = {
-                            status: 1,
-                            message: "Success upload new sub service to server",
-                            // data: records['ops'][0]
-                        };
-                        console.log(status);
-                        callback(status);
-                    }
-                });
-            }
+
+                }else {
+                    console.log(recodeId + " New Insert");
+                // Insert new service record
+                    collection.insert(addService, function (err, records) {
+                        if (err) {
+                            console.log(err);
+                            var status = {
+                                status: 0,
+                                message: "Failed"
+                            };
+                            console.log(status);
+                            callback(status);
+                        } else {
+                            var status = {
+                                status: 1,
+                                message: "Success upload new sub service to server",
+                                // data: records['ops'][0]
+                            };
+                            console.log(status);
+                            callback(status);
+                        }
+                    });
+                }
+            });
         });
     },
+
+
 
 
     getUserService: function (req, callback) {
