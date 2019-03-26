2d3510b2490728bf8c4b68338028ac9770f1802a
ketan patel <ketan_patel25@yahoo.com>
2019-03-26 09:52:04 +0530

    Add New Api

---
+++ models/CustomerModel.js
@@ -0,0 +1,198 @@
+var mongo = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
+var ObjectID = require('mongodb').ObjectID;
+var config = require('../db_config.json');
+var comman = require('../models/Comman');
+var bcrypt = require('bcrypt');
+
+
+var Customer  = {
+    addNewUser: function (req, callback) {
+        comman.getNextSequenceUserID("cu_user", function (result) {
+            //  console.log(result);
+            var newUser = {
+                cu_id: "CU0" + result,
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                email: req.body.email,
+                dob: req.body.dob,
+                gender: req.body.gender,
+                mobile_no: req.body.mobile_no,
+                password: req.body.password,
+                creationDate: new Date().toISOString()
+            };
+
+            mongo.connect(config.dbUrl, {useNewUrlParser: true}, function (err, db) {
+                var collectionCU = db.db(config.dbName).collection(config.collections.cu_profile);
+                collectionCU.find({email: req.body.email}).toArray(function (err, docs) {
+                    if (err) {
+                        console.log(err);
+                        var status = {
+                            status: 0,
+                            message: "Failed"
+                        };
+                        console.log(status);
+                        callback(status);
+                    } else {
+                        // assert.equal(1, docs.length);
+                        if (docs.length == 1) {
+                            var status = {
+                                status: 0,
+                                message: "This Email id already register",
+                            };
+                            console.log(status);
+                            callback(status);
+                        } else {
+                            bcrypt.hash(req.body.password, 10, function (err, hash) {
+                                newUser.password = hash;
+                                var collectionSP = db.db(config.dbName).collection(config.collections.cu_profile);
+                                collectionSP.insert(newUser, function (err, records) {
+                                    if (err) {
+                                        console.log(err);
+                                        var status = {
+                                            status: 0,
+                                            message: "Failed"
+                                        };
+                                        console.log(status);
+                                        callback(status);
+                                    } else {
+                                        var status = {
+                                            status: 1,
+                                            message: "Success create new user",
+                                            data: records['ops'][0]
+                                        };
+                                        console.log(status);
+                                        callback(status);
+                                    }
+                                });
+                            });
+                        }
+                    }
+                });
+            });
+        });
+
+
+    },
+
+
+
+
+    checkCUUserCreated: function (req, callback) {
+        var mobile_no = req.body.mobile_no;
+        mongo.connect(config.dbUrl, {useNewUrlParser: true}, function (err, db) {
+            var collectionSP = db.db(config.dbName).collection(config.collections.cu_profile);
+            collectionSP.find({mobile_no: mobile_no}).toArray(function (err, docs) {
+                if (err) {
+                    console.log(err);
+                    var status = {
+                        status: 0,
+                        message: "Failed"
+                    };
+                    console.log(status);
+                    callback(status);
+                } else {
+                    // assert.equal(1, docs.length);
+                    if (docs.length == 1) {
+                        var status = {
+                            status: 1,
+                            message: "Successfully data getting",
+                            data: docs[0]
+
+                        };
+                    } else {
+                        var status = {
+                            status: 0,
+                            message: "No User"
+                        };
+                    }
+
+                    console.log(status);
+                    callback(status);
+                }
+            });
+        });
+    },
+
+
+
+    CUUserLogin: function (req, callback) {
+
+        var email = req.body.email;
+        var password = req.body.password;
+
+        mongo.connect(config.dbUrl, {useNewUrlParser: true}, function (err, db) {
+            var collectionCU = db.db(config.dbName).collection(config.collections.cu_profile);
+            collectionCU.find({email: email}).toArray(function (err, docs) {
+                if (err) {
+                    console.log(err);
+                    var status = {
+                        status: 0,
+                        message: "Failed"
+                    };
+                    console.log(status);
+                    callback(status);
+                } else {
+                    console.log(docs);
+                    if (docs.length == 1) {
+
+                        bcrypt.compare(password, docs[0].password, function(err, res) {
+                            if(res) {
+                                // Passwords match
+                                var status = {
+                                    status: 1,
+                                    message: "Successfully data getting",
+                                    data: docs[0]
+                                };
+                                console.log(status);
+                                callback(status);
+
+                            } else {
+                                var status = {
+                                    status: 0,
+                                    message: "User Id and Passwords don't match"
+                                };
+                                console.log(status);
+                                callback(status);
+                                // Passwords don't match
+                            }
+                        });
+
+
+                    } else {
+                        var status = {
+                            status: 0,
+                            message: "This Email id are not register."
+                        };
+                        console.log(status);
+                        callback(status);
+                    }
+                }
+            });
+        });
+    },
+
+    CURegiCheck: function (req, callback) {
+
+        var cu_id = req.body.cu_id;
+        var checkCU = {
+            cu_personal: false,
+        };
+
+        mongo.connect(config.dbUrl, {useNewUrlParser: true}, function (err, db) {
+            var collectionSP = db.db(config.dbName).collection(config.collections.cu_profile);
+            collectionSP.findOne({cu_id: cu_id}, function (err, docs) {
+                if (docs != null) {
+                    checkCU.cu_personal = true;
+                } else {
+                    checkCU.cu_personal = false;
+                }
+                console.log(checkCU.cu_personal);
+                console.log(checkCU);
+                callback(checkCU);
+            });
+        });
+    },
+
+}
+module.exports = Customer ;
---
+++ routes/customer.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var router = express.Router();
+var customerModel = require('../models/CustomerModel.js');
+var configDB = require('../db_config.json');
+
+const multerSettings = require("../models/Multer-settings");
+const Bluebird = require("bluebird");
+let uploadSPWork = multerSettings.uploadSPWork;
+let uploadSPUserProfileIM = multerSettings.uploadSPUserProfileIM;
+
+
+// //G E T   M E T H O D S
+
+
+//P O S T   M E T H O D S
+
+router.post('/AddNewUser', function (req, res, next) {
+    customerModel.addNewUser(req, function (err, result) {
+        if (err) {
+            res.json(err);
+            console.log(err);
+        } else {
+            console.log(result);
+            res.json(result);//or return count for 1 & 0
+
+        }
+    });
+});
+
+
+
+
+router.post('/checkCUUserCreated', function (req, res, next) {
+    customerModel.checkCUUserCreated(req, function (err, result) {
+        if (err) {
+            res.json(err);
+            console.log(err);
+        } else {
+            console.log(result);
+            res.json(result);//or return count for 1 & 0
+        }
+    });
+});
+
+router.post('/CPUserRegistrationCheck', function (req, res, next) {
+    console.log("call CUUserRegistrationCheck -----1");
+    customerModel.CURegiCheck(req, function (err, result) {
+        if (err) {
+            res.json(err);
+            console.log(err);
+        } else {
+            console.log(result);
+            res.json(result);//or return count for 1 & 0
+        }
+    });
+});
+
+
+
+
+router.post('/CUUserLogin', function (req, res, next) {
+    customerModel.CUUserLogin(req, function (err, result) {
+        if (err) {
+            res.json(err);
+            console.log(err);
+        } else {
+            console.log(result);
+            res.json(result);//or return count for 1 & 0
+        }
+    });
+});
+
+
+
+
+
+module.exports = router;
