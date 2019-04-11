d8b1c67b94731c415dbfcbadddd3aa8c9c614ff2
ketan patel <ketan_patel25@yahoo.com>
2019-04-11 18:35:15 +0530

    Add New Api

--- models/CustomerModel.js
+++ models/CustomerModel.js
@@ -363,6 +363,7 @@ var Customer = {
                                             discountAfterPrice: discountAfterPrice,
                                             dist: element.dist,
                                             sp_about:docs[0].userprofile[0].about_sp_profile,
+                                            sp_workImage:docs[0].userprofile[0].workImages,
                                             avg_response:docs[0].userprofile[0].avg_response,
                                             avg_rating:docs[0].userprofile[0].avg_rating
 
