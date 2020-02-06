2926df86ff6bbcc2df333d06995c7ce5d0a143a8
ketan patel <ketan_patel25@yahoo.com>
2020-02-06 15:48:14 +0530

    Add New Api

--- models/CustomerModel.js
+++ models/CustomerModel.js
@@ -805,84 +805,106 @@ var Customer = {
 
     },
 
-
     addServiceAlertData: function (req, callback) {
-        var cp_alert_id = req.body.cp_alert_id;
-        // if (cp_alert_id != null) {
-        //     comman.DeletedAlertService(req.body.cp_alert_id, function (result) {
-        //         console.log(result);
-        //     });
-        // }
-
-
-        comman.getNextSequenceUserID("cu_alert_id", function (result) {
-            //  console.log(result);
-
-            comman.getCustomerData(req.body.cu_id, function (CU_data) {
-
-                var newServiceAlert = {
-                    cp_alert_id: "CP-ALERT0" + result,
-                    comment: req.body.comment,
-                    address: req.body.address,
-                    sr_id: req.body.sr_id,
-                    sr_title: req.body.sr_title,
-                    cost_item: req.body.cost_item,
-                    cu_id: req.body.cu_id,
-                    cu_first_name: CU_data.first_name,
-                    cu_last_name: CU_data.last_name,
-                    mobile_no: CU_data.mobile_no,
-                    cc_ids: req.body.cc_ids,
-                    sr_type: req.body.sr_type,
-                    type_of_service: req.body.type_of_service,
-
-                    location: {
-                        coordinates: [parseFloat(req.body.coordinatePoint.longitude), parseFloat(req.body.coordinatePoint.latitude)],
-                        type: "Point"
-                    },
-                    alert_active: req.body.alert_active,
-                    creationDate: new Date().toUTCString()
-                };
-                mongo.connect(config.dbUrl, {useNewUrlParser: true}, function (err, db) {
-                    var collectionSP = db.db(config.dbName).collection(config.collections.cu_service_alert);
-                    // collectionSP.insert(newServiceAlert, function (err, records) {
-                    //     if (err) {
-                    //         console.log(err);
-                    //         var status = {
-                    //             status: 0,
-                    //             message: "Failed !. Server Error....."
-                    //         };
-                    //         console.log(status);
-                    //         callback(status);
-                    //     } else {
-
-                            if (cp_alert_id != null) {
-                                comman.deletedAlertService(req.body.cp_alert_id, function (resultDelete) {
-                                    console.log(resultDelete);
-
-                                    // var status = {
-                                    //     status: 1,
-                                    //     message: "Successfully add your Service alert information are store.",
-                                    //     // data: records
-                                    // };
-                                    // console.log(status);
-                                    callback(resultDelete);
-                                });
-                            }else {
-                                var status = {
-                                    status: 1,
-                                    message: "Successfully add your Service alert information are store.",
-                                    // data: records
-                                };
-                                console.log(status);
-                                callback(status);
-                            }
-                //         }
-                //     });
+        var cp_alert_id_old = req.body.cp_alert_id;
+        mongo.connect(config.dbUrl, {useNewUrlParser: true}, function (err, db) {
+            if (cp_alert_id_old != null) {
+                comman.deletedAlertService(req.body.cp_alert_id, function (resultDelete) {
+                    console.log(resultDelete);
+                    callback(resultDelete);
                 });
+            }else {
+                var status = {
+                    status: 1,
+                    message: "Successfully 555555555555555555.",
+                    // data: records
+                };
+                console.log(status);
+                callback(status);
+            }
 
-            });
         });
     },
+
+
+
+    // addServiceAlertData: function (req, callback) {
+    //     var cp_alert_id_old = req.body.cp_alert_id;
+    //     // if (cp_alert_id != null) {
+    //     //     comman.DeletedAlertService(req.body.cp_alert_id, function (result) {
+    //     //         console.log(result);
+    //     //     });
+    //     // }
+    //
+    //
+    //     comman.getNextSequenceUserID("cu_alert_id", function (result) {
+    //         //  console.log(result);
+    //
+    //         comman.getCustomerData(req.body.cu_id, function (CU_data) {
+    //
+    //             var newServiceAlert = {
+    //                 cp_alert_id: "CP-ALERT0" + result,
+    //                 comment: req.body.comment,
+    //                 address: req.body.address,
+    //                 sr_id: req.body.sr_id,
+    //                 sr_title: req.body.sr_title,
+    //                 cost_item: req.body.cost_item,
+    //                 cu_id: req.body.cu_id,
+    //                 cu_first_name: CU_data.first_name,
+    //                 cu_last_name: CU_data.last_name,
+    //                 mobile_no: CU_data.mobile_no,
+    //                 cc_ids: req.body.cc_ids,
+    //                 sr_type: req.body.sr_type,
+    //                 type_of_service: req.body.type_of_service,
+    //
+    //                 location: {
+    //                     coordinates: [parseFloat(req.body.coordinatePoint.longitude), parseFloat(req.body.coordinatePoint.latitude)],
+    //                     type: "Point"
+    //                 },
+    //                 alert_active: req.body.alert_active,
+    //                 creationDate: new Date().toUTCString()
+    //             };
+    //             mongo.connect(config.dbUrl, {useNewUrlParser: true}, function (err, db) {
+    //                 var collectionSP = db.db(config.dbName).collection(config.collections.cu_service_alert);
+    //                 // collectionSP.insert(newServiceAlert, function (err, records) {
+    //                 //     if (err) {
+    //                 //         console.log(err);
+    //                 //         var status = {
+    //                 //             status: 0,
+    //                 //             message: "Failed !. Server Error....."
+    //                 //         };
+    //                 //         console.log(status);
+    //                 //         callback(status);
+    //                 //     } else {
+    //
+    //                         if (cp_alert_id_old != null) {
+    //                             comman.deletedAlertService(req.body.cp_alert_id, function (resultDelete) {
+    //                                 console.log(resultDelete);
+    //
+    //                                 // var status = {
+    //                                 //     status: 1,
+    //                                 //     message: "Successfully add your Service alert information are store.",
+    //                                 //     // data: records
+    //                                 // };
+    //                                 // console.log(status);
+    //                                 callback(resultDelete);
+    //                             });
+    //                         }else {
+    //                             var status = {
+    //                                 status: 1,
+    //                                 message: "Successfully add your Service alert information are store.",
+    //                                 // data: records
+    //                             };
+    //                             console.log(status);
+    //                             callback(status);
+    //                         }
+    //             //         }
+    //             //     });
+    //             });
+    //
+    //         });
+    //     });
+    // },
 
     searchQuoteProvider: function (req, callback) {
         var sr_id = req.body.sr_id;
